ffedf940c2cd848d3e8926f68bb10b65e324ec6e
santhosh04me <santhosh04me@gmail.com>
2023-10-09 20:19:45 +0530

    generic <T>

---
+++ src/collection/Gen2.java
@@ -0,0 +1,13 @@
+package collection;
+
+public class Gen2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Gentric<String> gd =new Gentric<String>("santhosh");
+		Gentric<Integer> gd2 =new Gentric<Integer>(123);
+		gd.show();
+		gd2.show();
+	}
+
+}
