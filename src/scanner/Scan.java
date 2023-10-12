4f1d41fce0a5509a8ec6b174b163c59d7c1946e6
santhosh04me <santhosh04me@gmail.com>
2023-10-12 19:51:01 +0530

    scanner

---
+++ src/scanner/Scan.java
@@ -0,0 +1,23 @@
+package scanner;
+
+import java.util.Scanner;
+
+public class Scan {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+			int no1=sc.nextInt();
+			int no2=sc.nextInt();
+			System.out.println(no1);
+			System.out.println(no2);
+			System.out.println(no1+no2);
+			float no3=sc.nextFloat();
+			System.out.println(no3);
+			String no4=sc.next();
+			System.out.println(no4);
+			String no5=sc.nextLine();
+			System.out.println(no5);
+	}
+
+}
